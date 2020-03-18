1b8ff46549b7ab079282f6571592a76c124d5954
Maria Connie Sanchez <msnchz04@gmail.com>
2020-03-18 19:59:46 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,18 @@
 class Customer
+  attr_accessor :name, :age 
+  @@all = []
   
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end 
+  
+  def new_meal
+    
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 end
